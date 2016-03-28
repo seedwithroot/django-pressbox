f718ee31d69ab7b63b44a3f582f2b38290bbaa17
Justin Lane <justinlane@Audience-2.local>
2016-03-28 10:01:41 -0700

    pinterest pin

--- pressbox/urls.py
+++ pressbox/urls.py
@@ -3,5 +3,5 @@ from pressbox.views import press_detail, press_list
 
 
 urlpatterns = [url(r'^(?P<slug>[-\w]+)/$',press_detail,name= 'press_detail'),
-url(r'^$',press_list, name = 'press_list'),
+url(r'^$',press_list, name = 'pressroom_home'),
 ]
