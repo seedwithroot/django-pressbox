67db660796d560680e9a5c81eba83d3eb5578037
Justin Lane <justinlane@Audience-2.local>
2016-03-28 08:41:30 -0700

    urls to Django 1.9+

--- pressbox/urls.py
+++ pressbox/urls.py
@@ -1,17 +1,18 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import include, url
+
 from pressbox.views import press_detail, press_list
 
 
 urlpatterns = patterns('',
 
-url(r'^(?P<slug>[-\w]+)/$', 
-    view = press_detail, 
+url(r'^(?P<slug>[-\w]+)/$',
+    view = press_detail,
     name= 'press_detail'),
 
-url(r'^$', 
-    view = press_list, 
+url(r'^$',
+    view = press_list,
     name = 'press_list'),
-    
+
 )
 
 #TODO:
